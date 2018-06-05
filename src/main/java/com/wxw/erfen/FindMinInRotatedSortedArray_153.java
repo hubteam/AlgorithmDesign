3c649f1e5a8c725b6e9f5b964118b611fb674450
xinweiwang <10187@192.168.0.107>
2018-06-06 01:41:40 +0800

    二分算法完善

--- src/main/java/com/wxw/erfen/FindMinInRotatedSortedArray_153.java
+++ src/main/java/com/wxw/erfen/FindMinInRotatedSortedArray_153.java
@@ -7,16 +7,38 @@ package com.wxw.erfen;
  */
 public class FindMinInRotatedSortedArray_153 {
 
-//    public static int findMinElement(int[] nums){
-//        int l = 0, h = nums.length - 1;
-//        while (l <= h){
-//            int m = (l + h) / 2;
-////            if (nums[m] )
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        int[] nums = {3,4,5,1,2};
-//        System.out.println(findMinElement(nums));
-//    }
+    public static int findMinElement(int[] nums){
+    	int l = 0, h = nums.length - 1;
+        while (l < h) {        	
+            int m = l + (h - l) / 2;
+            
+            if (nums[m] == nums[l] && nums[m] == nums[h]) {
+				//顺序查找
+            	for (int i = 0; i < nums.length - 1; i++) {
+					if (nums[i] < nums[i+1]) {
+						return nums[i];
+					}
+				}
+			}
+            
+            if (nums[m] <= nums[h])
+                h = m;
+            else
+                l = m + 1;
+        }
+        return nums[l];
+    }
+
+    public static void main(String[] args) {
+    	int[] a = {6,1,2,3,4,5};
+		int[] b = {1,2,3,4,5,6,7,8};
+		int[] c = {1,0,1,1,1};
+		int[] d = {1,1,1,0,1};
+		int[] e = {4,5,1,2,3};
+        System.out.println(findMinElement(a));
+        System.out.println(findMinElement(b));
+        System.out.println(findMinElement(c));
+        System.out.println(findMinElement(d));
+        System.out.println(findMinElement(e));
+    }
 }
--- src/main/java/com/wxw/erfen/SearchRange_34.java
+++ src/main/java/com/wxw/erfen/SearchRange_34.java
@@ -8,10 +8,29 @@ public class SearchRange_34 {
 
     public static int[] findRange(int[] nums, int key){
         int start = LeftLocInReapeatArray.leftLocation(nums, key);
-        int end = LeftLocInReapeatArray.leftLocation(nums, key + 1);
+        int end = LeftLocInReapeatArray.leftLocation(nums, key + 1) - 1;
 
-        int[] res = new int[]{start, end};
-        return res;
-
+        if (start == nums.length || nums[start] != key) {
+        	return new int[] {-1, -1};
+        } else {
+        	//别人的代码是：new int[] {start, Math.max(start, end)}
+        	return new int[] {start, end};
+        }
     }
+    
+    /**
+     * Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+     * @param args
+     */
+    public static void main(String[] args) {
+		int[] nums = {5,7,7,8,8,10};
+		int[] res1 = findRange(nums, 8);
+		int[] res2 = findRange(nums, 6);
+		System.out.println("[" + res1[0] + "," + res1[1] + "]");
+		System.out.println("[" + res2[0] + "," + res2[1] + "]");
+	}
 }
